c3e5433ac130b898b73c0c21e266b0484a051aa0
Leonardo Petribú <leonardo.brennand@ufpe.br>
2025-11-21 18:47:07 -0300

    feat(my_first_bash_script.sh): Atualização do script .sh

--- my_first_bash_script.sh
+++ my_first_bash_script.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-nome="Léo"
+echo "Hello! What's your name?"
 
-echo "Olá, $nome!"
+read name
+
+echo "Hi, $name! Insert the math operation you want me to calculate:"
+
+read operation
+
+
+if [ $((operation)) -gt 0 ]; then
+	signal='positive'
+elif [ $((operation)) -lt 0 ]; then
+	signal='negative'
+else
+	signal='zero'
+fi
+
+
+echo "Right on! The result is $((operation)) and the number is $signal. Am I right?"
+
+read usr_confirm
+
+
+if [ $usr_confirm == 'yes' ]; then
+	echo "Yay!"
+elif [ $usr_confirm == 'no' ]; then
+	echo "I'm sorry! :("
+else
+	echo "I don't get it! Could you please retype it? Just put 'yes' or 'no'."
+fi
